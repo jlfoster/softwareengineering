20e200606bebe24d9501d8b8b6d00e527194d17e
Jodie Foster <jodielynnfoster@hotmail.ca>
2012-11-08 10:39:41 -0800

    Created bill class in order to provide a subtotal

---
+++ 310A4/src/org/jlfoster/cosc310/restaraunt/Bill.java
@@ -0,0 +1,24 @@
+package org.jlfoster.cosc310.restaraunt;
+
+import java.util.ArrayList;
+
+public class Bill {
+	double subtotal = 0;
+
+
+	public void setBill(ArrayList<MenuItem> AL){
+		//print out the titles
+		System.out.println("Menu Item				Price");
+		//for each item that they've selected
+		for(MenuItem item : AL){
+			//print out the name and price
+			System.out.println(item.getName() +"		" + item.getPrice());
+			//update the subtotal
+			subtotal = subtotal + item.getPrice();
+		}
+		//pring out the subtotal
+		System.out.println("Subtotal: " + subtotal);
+
+
+	}
+}
