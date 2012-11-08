ec274a351eed6a76de74a1556e08a1db300fc651
Carson Myers <carsonmyers@gmail.com>
2012-11-08 09:48:09 -0800

    Added restaraunt and menuitem

---
+++ 310A4/src/org/jlfoster/cosc310/restaraunt/MenuItem.java
@@ -0,0 +1,27 @@
+package org.jlfoster.cosc310.restaraunt;
+
+public class MenuItem
+{
+	private String name;
+	private double price;
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public double getPrice()
+	{
+		return price;
+	}
+	
+	public void setPrice(double price)
+	{
+		this.price = price;
+	}
+}
